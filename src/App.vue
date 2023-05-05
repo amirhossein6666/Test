4ed2985f54c166801635fbd297308f022bce95dc
Your Name <you@example.com>
2023-05-05 19:45:51 +0330

    create vue with vue cli and learning slot props teleport and ....

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <input type="text" ref="name">
+  <button @click="addClass">add class</button>
+  <button @click="removeClass">remove class</button>
+  <button @click.shift="shwoTempOnly">show the temp contains (shift)</button>
+  <button @click="showSecondTemp">create another temp</button>
+  <button @click="changeShowTeleportContains"> show/hide teleport</button>
+  <div v-if="showTemp">
+    <Temp  theme = "sale"  @close="changeTempShow">
+      <template v-slot:links>
+        <p>using slot for pass links</p>
+        <a href="https://github.com/">github</a>
+        <a href="https://github.com/amirhossein6666">my github> </a>
+      </template>
+        <p>its a without name slot</p>
+      </Temp>
+  </div>
+  <div v-if="ShowSecondTemp">
+    <Temp @close=changeSecondTempShow>
+      <p>second Temp with slots</p>
+      <template v-slot:image>
+        <img src="./assets/images/download.png" alt="ying yong">
+        <img src="./assets/images/download.jpeg" alt="Soghrat">
+      </template>
+    </Temp>
+  </div>
+  <teleport to=".teleport" v-if="ShowTeleport">
+    <!-- this section teleport to index.html -->
+    <p>use teleport</p>
+  </teleport>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Temp from './components/Temp.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Temp
+  },
+  data() {
+    return {
+      title : "amir",
+      showTemp : false,
+      ShowSecondTemp : false,
+      ShowTeleport : false
+    }
+  },
+  methods : {
+    addClass() {
+      this.$refs.name.classList.add('active')
+      console.log(this.$refs.name)
+    },
+    removeClass() {
+      this.$refs.name.classList.remove('active')
+      console.log(this.$refs.name)
+    },
+    changeTempShow() {
+      this.showTemp = !this.showTemp
+    },
+    shwoTempOnly() {
+      this.showTemp = true
+    },
+    showSecondTemp() {
+      this.ShowSecondTemp = true
+    },
+    changeSecondTempShow() {
+      this.ShowSecondTemp = !this.ShowSecondTemp
+    },
+    changeShowTeleportContains() {
+      this.ShowTeleport = !this.ShowTeleport
+    }
   }
-}
-</script>
+  }
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
---
+++ src/components/Temp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="{sale : theme === 'sale'}" class="color">
+    <p>this text is from temp</p>
+  </div>
+  <button @click="closeTemp">close temp</button>
+  <div>
+    <slot name="links"></slot>
+    <slot name="image"></slot>
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props : ['theme'],
+  methods : {
+    closeTemp() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style>
+.color.sale {
+  color: red;
+}
+</style>
+
+
